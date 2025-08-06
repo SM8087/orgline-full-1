070335a3eb64fc61c16526b6e11f87200aca15c8
SM8087 <sm@orglineai.com>
2025-08-06 15:18:02 +0530

    updated font and added repo for mydashboard

--- OrgLineAI/src/app/app.routes.ts
+++ OrgLineAI/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Register } from './auth/register/register';
 import { Login } from './auth/login/login';
+import { MyDashboard } from './my-dashboard/my-dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  { path: 'mydashboard', component: MyDashboard}
 ];
